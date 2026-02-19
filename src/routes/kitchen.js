d281d7adfdf9b37c29ced0141a270e0649d36547
Sayain-Aditya <as4316664@gmail.com>
2026-02-19 14:49:10 +0530

    asd

--- src/routes/kitchen.js
+++ src/routes/kitchen.js
@@ -13,13 +13,13 @@ const tenantMiddleware = require('../middleware/tenant');
 const router = express.Router();
 
 // Get kitchen orders
-router.get('/all/kitchen/orders', auth(['KITCHEN_STAFF', 'MANAGER', 'RESTAURANT_ADMIN']), tenantMiddleware, getKitchenOrders);
+router.get('/all/kitchen/orders', auth(['CHEF', 'KITCHEN_STAFF', 'MANAGER', 'RESTAURANT_ADMIN']), tenantMiddleware, getKitchenOrders);
 
 // Update order status
-router.patch('/update/orders/status/:id', auth(['KITCHEN_STAFF', 'MANAGER', 'RESTAURANT_ADMIN']), tenantMiddleware, updateOrderStatus);
+router.patch('/update/orders/status/:id', auth(['CHEF', 'KITCHEN_STAFF', 'MANAGER', 'RESTAURANT_ADMIN']), tenantMiddleware, updateOrderStatus);
 
 // Update KOT status
-router.patch('/update/kot/status/:id', auth(['KITCHEN_STAFF', 'MANAGER', 'RESTAURANT_ADMIN']), tenantMiddleware, updateKOTStatus);
+router.patch('/update/kot/status/:id', auth(['CHEF', 'KITCHEN_STAFF', 'MANAGER', 'RESTAURANT_ADMIN']), tenantMiddleware, updateKOTStatus);
 
 // Update KOT priority
 router.patch('/update/kot/priority/:id', auth(['MANAGER', 'RESTAURANT_ADMIN']), tenantMiddleware, updateKOTPriority);
@@ -28,6 +28,6 @@ router.patch('/update/kot/priority/:id', auth(['MANAGER', 'RESTAURANT_ADMIN']), 
 router.patch('/update/orders/priority/:id', auth(['MANAGER', 'RESTAURANT_ADMIN']), tenantMiddleware, setPriority);
 
 // Print KOT for order
-router.post('/orders/:id/print-kot', auth(['KITCHEN_STAFF', 'MANAGER', 'RESTAURANT_ADMIN']), tenantMiddleware, printOrderKOT);
+router.post('/orders/:id/print-kot', auth(['CHEF', 'KITCHEN_STAFF', 'MANAGER', 'RESTAURANT_ADMIN']), tenantMiddleware, printOrderKOT);
 
 module.exports = router;
